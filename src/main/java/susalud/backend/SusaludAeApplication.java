61e5081f455aecfed25e72f1171c962248239a51
Edwin Didier Méndez Rojas - Ceiba Software <edwin.mendez@ceiba.com.co>
2019-03-22 13:52:14 -0500

    setting eureka-server

--- src/main/java/susalud/backend/SusaludAeApplication.java
+++ src/main/java/susalud/backend/SusaludAeApplication.java
@@ -1,12 +1,10 @@
 package susalud.backend;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@EnableAutoConfiguration // defines this as a Spring Boot application
 @EnableDiscoveryClient
 @SpringBootApplication
 public class SusaludAeApplication {
