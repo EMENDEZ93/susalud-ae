a612b8a8322c3838b6b702a79150b1c3df1e0744
Edwin Didier Méndez Rojas - Ceiba Software <edwin.mendez@ceiba.com.co>
2019-03-26 15:16:47 -0500

    ambientes config

--- src/main/java/susalud/backend/config/ambientes/AmbientesConfig.java
+++ src/main/java/susalud/backend/config/ambientes/AmbientesConfig.java
@@ -49,27 +49,18 @@ public class AmbientesConfig {
 	@Profile("dev")
 	@Bean
 	public String devDatabaseConnection() {
-		System.out.println("DB connection for DEV - H2");
-		System.out.println(driverClassName);
-		System.out.println(url);
 		return "DB connection for DEV - H2";
 	}
 
 	@Profile("test")
 	@Bean
 	public String testDatabaseConnection() {
-		System.out.println("DB Connection to RDS_TEST - Low Cost Instance");
-		System.out.println(driverClassName);
-		System.out.println(url);
 		return "DB Connection to RDS_TEST - Low Cost Instance";
 	}
 
 	@Profile("prod")
 	@Bean
 	public String prodDatabaseConnection() {
-		System.out.println("DB Connection to RDS_PROD - High Performance Instance");
-		System.out.println(driverClassName);
-		System.out.println(url);
 		return "DB Connection to RDS_PROD - High Performance Instance";
 	}
 
